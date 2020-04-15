9bf45c2fd9aee110bf4539e4990da140ea5cc9e9
suneelchitteti <suneelchitteti695@gmail.com>
2020-04-15 11:51:47 +0530

    firstcommit

---
+++ image_uploadproject/imageapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+class Hotel(models.Model):
+    name = models.CharField(max_length=50)
+    hotel_Main_Img = models.ImageField(upload_to='images/')
+
+# Create your models here.
